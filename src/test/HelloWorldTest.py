068d41be58727c0b4fe75d4bfc275c3aa883a9cf
Kaveri Priya <kaveri.priya@revature.com>
2024-02-21 21:46:20 +0530

    Labs and Test complete

---
+++ src/test/HelloWorldTest.py
@@ -0,0 +1,19 @@
+# HelloWorldTest.py
+import unittest
+from ..main.lab import Lab
+
+
+class HelloWorldTest(unittest.TestCase):
+    def setUp(self):
+        self.hw = Lab()
+
+    def test_hello(self):
+        """
+        Method sayHello() must return "Hello, World!". The strip method will remove any extra spaces or newlines at the end.
+        """
+        expected = "Hello, world!"
+        actual = self.hw.sayHello().strip()
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
